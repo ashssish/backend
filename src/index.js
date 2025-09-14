4139457d4f9eca52b5d7e9e7335a138c18f09878
ashish <ashishdevtech1@gmail.com>
2025-09-14 17:16:08 +0530

    connect to mongo atles

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import dotenv from "dotenv";
+
+import mongoose from "mongoose";
+import { DB_NAME } from "./constants.js";
+import connectDB from "../db/db.js";
+
+dotenv.config({ path: "./.env" });
+
+connectDB();
