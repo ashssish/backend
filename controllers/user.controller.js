5d55c013eebe9c7881085e4d87f699fae68a6815
ashish <ashishdevtech1@gmail.com>
2025-09-20 12:43:30 +0530

    change password, get current user, update account details, update avtar and cover image

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -221,4 +221,104 @@ const refreshAccessToken = asyncHndler(async (req, res) => {
   }
 });
 
-export { registerUser, loginUser, logoutUser, refreshAccessToken };
+const changeCurrentPassword = asyncHndler(async (req, res) => {
+  const { oldPassword, newPassword } = req.body;
+
+  const user = await User.findById(req.user?._id);
+  const isPasswordCorrect = user.isPasswordCorrect(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new ApiError(400, "Invailid Old Password");
+  }
+
+  user.password = newPassword;
+  await user.save({ validateBeforeSave: false });
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, {}, "Password change successfully"));
+});
+
+const getCurrentUser = asyncHndler(async (req, res) => {
+  return res.status(200).json(200, req.user, "current user fetch successfully");
+});
+
+const updateAccountDetails = asyncHndler(async (req, res) => {
+  const { fullname, email } = req.body;
+
+  if (!fullname || !email) {
+    throw new ApiError(400, "All field are required");
+  }
+
+  const user = User.findByIdAndUpdate(
+    req.user?._id,
+    {
+      $set: { fullname, email: email },
+    },
+    { new: true }
+  ).select("-password"); //we can use fullname and email both kind like this
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, user, "Account details successfully"));
+});
+
+const updatUserAvtar = asyncHndler(async (req, res) => {
+  const avtarLocalPath = req.file?.path;
+
+  if (!avtarLocalPath) {
+    throw new ApiError(400, "Avtar file is missing");
+  }
+
+  const avtar = await uploadOnCloudinary(avtarLocalPath);
+
+  if (!avtar.url) {
+    throw new Error(400, "Error While Uploading on Avtar");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    { $set: { avatar: avtar.url } },
+    { new: true }.select("-password")
+  );
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, user, "avtar image update successfully"));
+});
+
+const updatUserCoverImage = asyncHndler(async (req, res) => {
+  const coverImageLocalPath = req.file?.path;
+
+  if (!coverImageLocalPath) {
+    throw new ApiError(400, "Cover image file is missing");
+  }
+
+  const coverImage = await uploadOnCloudinary(coverImageLocalPath);
+
+  if (!coverImage.url) {
+    throw new Error(400, "Error While Uploading on Cover image");
+  }
+
+  const user = await User.findByIdAndUpdate(
+    req.user?._id,
+    { $set: { coverImage: coverImage.url } },
+    { new: true }.select("-password")
+  );
+
+  return res
+    .status(200)
+    .json(new apiResponse(200, user, "cover image update successfully"));
+});
+
+export {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser,
+  updateAccountDetails,
+  updatUserAvtar,
+  updatUserCoverImage,
+};
